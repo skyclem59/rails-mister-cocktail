f50dc74fea6009b227a56a8b3f1a228b35e957a7
clement scailteux <clement.scailteux@csreseaux.com>
2018-02-23 16:36:14 +0100

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :cocktails, only: [:show, :index, :new, :create ] do
+    resources :doses, only: [:new, :create ]
+  end
+  resources :doses, only: [:destroy]
+  root to: 'cocktails#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
